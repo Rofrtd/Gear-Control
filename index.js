4c3bbe335e16198890c7db984a475db4e5f290c5
Romulo Furtado <romulo.furtado@hotmail.com>
2019-08-07 22:39:35 +1000

    feat(projects): react router setup

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
 app.use(express.static('public'))
+
+app.use((req, res) => {
+    // falls back 404 to index.html
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
