cc77e45138f136cd94778a5189a7790f27dab42d
Romulo Furtado <romulo.furtado@hotmail.com>
2019-08-07 20:14:25 +1000

    feat(infra): bulma setup

--- static/index.js
+++ static/index.js
@@ -1,4 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Fabio</div>, document.getElementById('app'))
+
+function App() {
+    return (
+        <div> <section className="section">
+            <div className="container">
+                <h1 className="title">Hello World</h1>
+                <p className="subtitle">My first website with <strong>Bulma</strong>! </p>
+            </div>
+        </section>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
