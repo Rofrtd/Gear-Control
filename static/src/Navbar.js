30c04076bcf32383183ea7a6bdd2d73b7bbf9556
Romulo Furtado <romulo.furtado@hotmail.com>
2019-08-13 18:35:23 +1000

    feat(nav): add logo and dummy buttons

--- static/src/Navbar.js
+++ static/src/Navbar.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import logo from './logo.svg'; 
 
 export default function Navbar() {
     return (
-            <nav className="navbar" role="navigation" aria-label="main navigation">
+            <nav className="navbar is-light" role="navigation" aria-label="main navigation">
                 <div className="navbar-brand">
                     <Link to="/" className="navbar-item"> 
-                        <span className= "icon is-large"> 
-                            <i className="fas fa-biohazard fa-3x"></i>
-                        </span>
+                        <img src={logo}/>
                     </Link>
 
                     <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
@@ -21,6 +20,22 @@ export default function Navbar() {
                 <div id="navbar-menu" className="navbar-menu">
                     <Link to="/" className="navbar-item">Home</Link>
                 </div>
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <Link to="/" >
+                            <i className="fas fa-bell"></i> 
+                        </Link>
+                    </div>
+                    <div className="navbar-item">
+                        <Link to="/" className="navbar-item">Equipment</Link>
+                    </div>
+                    <div className="navbar-item">
+                        <Link to="/" className="navbar-item">Users</Link>
+                    </div>
+                    <div className="navbar-item">
+                        <Link to="/" className="navbar-item">Reports</Link>
+                    </div>
+                </div> 
             </nav>
     )
 }
