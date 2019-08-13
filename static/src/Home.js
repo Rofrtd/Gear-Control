2e6141cb81f5bbeac85a03d44c71ec7aa7019d10
Romulo Furtado <romulo.furtado@hotmail.com>
2019-08-13 18:35:23 +1000

    feat(nav): add logo and dummy buttons

--- static/src/Home.js
+++ static/src/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Home(){
+export default function Home() {
     return (
-        <div>
-            <Link to="/add-project" className="button is-dark">Add New Project</Link>
+        <div className="columns">
+            <Link to="/add-project" className="button column is-link is-large">Add New Project</Link>
         </div>
     )
 }
