7e61189975ff7a07cd54fcef7c05fc40f26e03be
Romulo Furtado <romulo.furtado@hotmail.com>
2019-08-29 21:40:51 +1000

    fix(infra): configure env port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const uuidv1 = require('uuid/v1');
 const bodyParser = require('body-parser')
 
