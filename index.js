ab31789cdd6214002f35a376fc8c5254c062d25d
Romulo Furtado <romulo.furtado@hotmail.com>
2019-08-29 20:10:32 +1000

    refactor(projects): install knex

--- index.js
+++ index.js
@@ -2,26 +2,27 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
-const { Pool } = require('pg')
 const uuidv1 = require('uuid/v1');
 const bodyParser = require('body-parser')
 
-const pool = new Pool({  
-    connectionString: 'postgresql://postgres:@localhost:5432/gear-control'
+const knex = require('knex')({
+    client: 'pg',
+    connection: 'postgresql://postgres:@localhost:5432/gear-control'
 })
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
 
 app.get('/api/projects', async (req, res) => {
-    res.json((await pool.query("SELECT * FROM projects")).rows)
+    res.json((await knex('projects')))
 });
 
 app.post('/api/add-project', async (req, res) => {
    try {
-        await pool.query({
-            text: 'INSERT INTO projects(id, name, created_on) VALUES($1, $2 , NOW())',
-            values: [uuidv1(), req.body.name]
+        await knex('projects').insert({
+            id: uuidv1(),
+            name: req.body.name,
+            created_on: 'NOW()'
         })
     
         res.json({ message: "OK" });
