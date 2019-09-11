ab0d9128a6a5052c370178c2f19ff9aeaaadbd25
Romulo Furtado <romulo.furtado@hotmail.com>
2019-09-11 22:22:32 +1000

    feat(customers): add customers page

---
+++ static/src/AddCustomer.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import {Formik, Form, Field, ErrorMessage} from "formik";
+import * as Yup from 'yup';
+
+const ProjectSchema = Yup.object().shape({
+    name: Yup.string()
+        .min(2, "Too short!")
+        .max(25, "Too long!")
+        .required("Required!")
+})
+
+function onSubmit (history) {
+    return async (values) => {
+        const response = await fetch('/api/add-customer', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(values)
+        })
+
+        if(response.ok){
+            history.push("/add-customer")
+        } else {
+            console.log(await response.json());
+            alert("Customer not added!")
+        }
+    }
+}
+
+export default function AddCustomer (props){
+    const [customers, setCustomers] = useState([])
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch('/api/customers')
+            setCustomers(await response.json())
+        })()
+    }, [true])
+
+    return (
+        <div>
+            <Formik 
+                initialValues={{
+                    name: ''
+                }}
+                validationSchema={ProjectSchema}
+                onSubmit={onSubmit(props.history)}
+            >
+                <Form>
+                    <Field name="name" />
+                    <ErrorMessage name="name" />
+                    <button type="submit">Submit</button>
+                </Form>
+            </Formik>
+            <table> 
+                <tbody>
+                    {customers.map((customer) => (
+                        <tr key={customer.id}>
+                            <td>{customer.id}</td>
+                            <td>{customer.name}</td>
+                            <td>{customer.created_on}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
