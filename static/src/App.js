c81757d1255b54052be2bca1aada980d024916df
Romulo Furtado <romulo.furtado@hotmail.com>
2019-10-12 21:48:08 +1100

    feat(equipment): add equipment page route

--- static/src/App.js
+++ static/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import AddProject from './AddProject'
 import AddCustomer from './AddCustomer'
+import AddEquipment from './AddEquipment'
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
                     <Route path="/" exact component={Home} />
                     <Route path="/add-project" exact component={AddProject} />
                     <Route path="/add-customer" exact component={AddCustomer} />
+                    <Route path="/add-equipment" exact component={AddEquipment} />
                     <Route component={() => (<span> 404 </span>)} />
                 </Switch> 
             </div>
