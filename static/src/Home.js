d0554d1c39dfaefa112fd564267a533d16ec53a0
Romulo Furtado <romulo.furtado@hotmail.com>
2019-10-28 22:24:16 +1100

    feat(home): create cards for each project

--- static/src/Home.js
+++ static/src/Home.js
@@ -14,31 +14,40 @@ export default function Home() {
     
     return (
         <div>
-            <Link to="/add-project" className="button is-link is-large">Add New Project</Link>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Date</th>
-                        <th>Customer</th>
-                        <th>Start Date</th>
-                        <th>End Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {projects.map((project) => (
-                        <tr key={project.id}>
-                            <td>{project.id}</td>
-                            <td>{project.name}</td>
-                            <td>{project.created_on}</td>
-                            <td>{project.customer_id}</td>
-                            <td>{project.start_date}</td>
-                            <td>{project.end_date}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <div className="columns is-multiline"> 
+                {projects.map((project) => (
+                        <div key={project.id} className = "column is-one-quarter is-half-tablet">
+                            <div className="card">
+                                <header className="card-header">
+                                    <p className="card-header-title">{project.name}</p>
+                                </header>
+                            <div className="card-content">
+                                <div className="content">
+                                    ID: {project.id} 
+                                    <br/>
+                                    NAME: {project.name}
+                                    <br/>
+                                    CREATED ON: {project.created_on}
+                                    <br/>
+                                    Customer: {project.customer_id}
+                                    <br/>
+                                    START DATE: {project.start_date}
+                                    <br/>
+                                    END DATE: {project.end_date}
+                                    <br/>
+                                </div>
+                            </div>
+                            <footer className="card-footer">
+                                <a href="#" className="card-footer-item">Edit</a>
+                                <a href="#" className="card-footer-item">Close</a>
+                            </footer>
+                            </div>
+                        </div>
+                ))}
+            </div>
+            <Link to="/add-project" className="column button is-link is-large">
+                <i className="fa fa-plus"></i>
+            </Link>
         </div>
     )
 }
