2de710334b4a42b8c4ca32f1531ba33131e5142a
Romulo Furtado <romulo.furtado@hotmail.com>
2019-11-05 22:16:18 +1100

    fix(projects): alter migration for equipment_allocation table

--- migrations/20191030215203_add_allocated_equipment.js
+++ migrations/20191030215203_add_allocated_equipment.js
@@ -1,12 +1,16 @@
 exports.up = function(knex) {
     return knex.schema
-        .createTable('equipment-allocation', function (table) {
-            table.text('project_id').notNullable();
-            table.text('equipment_id').notNullable();
+        .createTable('equipment_allocation', function (table) {
+            table.uuid('project_id').notNullable();
+            table.uuid('equipment_id').notNullable();
+
+            table.foreign('project_id').references('projects.id');
+            table.foreign('equipment_id').references('equipment.id')
+            table.unique(['project_id', 'equipment_id'])
         })
 };
 
 exports.down = function(knex) {
     return knex.schema
-        .dropTable('equipment-allocation')
+        .dropTable('equipment_allocation')
 };
